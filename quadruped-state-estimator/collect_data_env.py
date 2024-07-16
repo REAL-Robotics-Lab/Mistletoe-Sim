051bf822653f972c2fabdad0e587563ca7e7f488
Easternspork <fumi.okutsu@gmail.com>
2024-07-16 16:48:29 +0900

    fix omse stuf

--- quadruped-state-estimator/collect_data_env.py
+++ quadruped-state-estimator/collect_data_env.py
@@ -123,7 +123,7 @@ class ObservationsCfg:
         """Observations for policy group."""
 
         # observation terms (order preserved)
-        base_lin_vel = ObsTerm(func=mdp.base_lin_vel, noise=None)
+        base_lin_vel = ObsTerm(func=mdp.base_lin_vel, noise=None )
         base_ang_vel = ObsTerm(func=mdp.base_ang_vel, noise=Unoise(n_min=-0.2, n_max=0.2))
         projected_gravity = ObsTerm(
             func=mdp.projected_gravity,
